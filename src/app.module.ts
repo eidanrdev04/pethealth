7f1addfc1007b30505b5cf11053c923136d94494
eidanrdev04 <eidanrdev04@gmail.com>
2024-07-25 23:28:04 -0400

    actualizar module de app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from 'src/modules/users/users.module';
 import { AuthModule } from 'src/modules/auth/auth.module';
+import { PetsModule } from './modules/pets/pets.module';
+import { VaccinationsModule } from './modules/vaccinations/vaccinations.module';
+import { VaccinationrecordsModule } from './modules/vaccinationrecords/vaccinationrecords.module';
+import { TreatmentsModule } from './modules/treatments/treatments.module';
+import { ActivitiesModule } from './modules/activities/activities.module';
+import { ConsultationsModule } from './modules/consultations/consultations.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal:true}),
-    UsersModule, AuthModule],
+    UsersModule, AuthModule, PetsModule, VaccinationsModule, VaccinationrecordsModule, TreatmentsModule, ActivitiesModule, ConsultationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
